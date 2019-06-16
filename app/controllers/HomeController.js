c0de980646d0f56760ef82d5ea44092214486f5d
Tofikhidayat <topikhidayat711@gmail.com>
2019-06-16 11:05:13 +0700

    claring app

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -14,19 +14,8 @@ class HomeController extends Controller {
 	async index({req,res,next}) {
 	 	let cake  =  await loadData("cake");
 	 	    cake  =  await Paginate(cake,9,"cake",req);
-		return await view("index");
-	}
-	// pagination
-	async pagination({req,res,next}) {
-	 	let cake  =  await loadData("cake");
-	 	    cake  =  await Paginate(cake,9,"cake",req);
-		return view("pagination",{cake:cake})
-	}
-	async store({req,res}) {
-		///console.log(req.body)
-		//return view("index");
-		return res.redirect(Routes.home)
-	} 
+		return view("index");
+	}	
 }
 
 module.exports = HomeController;
