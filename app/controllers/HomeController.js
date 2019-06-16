97095e1d176314df4033a6a7ce415419875d3778
Tofikhidayat <topikhidayat711@gmail.com>
2019-06-16 10:57:12 +0700

    add fix redner

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -5,40 +5,16 @@
 const Controller  =  use("app/Controller")
 const Paginate = use("/config/pagination").Paginate;
 const CrudMaster = use("/config/crud");
-const WelcomeMail  =  use("/app/mails/WelcomeMail");
-
-const crud  = new CrudMaster("world");
-crud.config();
-(async ()=>{
-	//console.log(await crud.all())
-	//await crud.store([{"name":"ahmed"} , {"name":"jiko"} , {"name":"keiko"}])
-
-
-
-
-	let lsm  =  await crud.all();
-		///lsm  = 	lsm.where("name","ahmed")
-		lsm  = lsm.whereMin("id",1)
-				  .whereMax("id",4)
-				  .where("name","ahmed") 
-		//console.log(lsm)
-	await crud.update("id",3,{
-		"name" : "tofik"
-	})
-
-})()
-
 
 class HomeController extends Controller {
 	constructor() {
 		super();
 	}
+	// index
 	async index({req,res,next}) {
-	 	///return res.redirect("bgst")
-	 	//return next();
 	 	let cake  =  await loadData("cake");
 	 	    cake  =  await Paginate(cake,9,"cake",req);
-		return view("index",{cake:cake})
+		return await view("index");
 	}
 	// pagination
 	async pagination({req,res,next}) {
